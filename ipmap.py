657b1d15ac6030056738886f44d8f3c281498a74
Nathael Arki <nathael.arki@supinternet.fr>
2017-11-29 14:27:05 +0100

    cache works pretty decently

--- ipmap.py
+++ ipmap.py
@@ -1,28 +1,33 @@
 import argparse
 import re
-import asyncio.subprocess
+# import asyncio.subprocess
 import asyncio
 # import xmltodict
 # import json
 from aiohttp import web
 
-def update_cache():
-    return True
+def update_cache(cache, ip, res):
+    cache[ip] = res
 
-def check_cache():
-    return True
+def check_cache(cache, ip):
+    return ip in cache
 
-async def scan_nmap(to_scan):
-    print("nmap -sV " + to_scan)
+async def scan_nmap(ip, cache):
+    print("nmap -sV " + ip)
+    if check_cache(cache, ip):
+        print(ip, ' cached')
+        result = cache[ip]
+    else:
+        print(ip, ' scanned')
+        # '-oX', '-',,
+        process = await asyncio.create_subprocess_exec(
+            'nmap', '-sV', ip,
+            stdout=asyncio.subprocess.PIPE)
 
-    # '-oX', '-',,
-    process = await asyncio.create_subprocess_exec(
-        'nmap', '-sV', to_scan,
-        stdout=asyncio.subprocess.PIPE)
+        stdout, _ = await process.communicate()
+        result = stdout.decode().strip()
+        update_cache(cache, ip, result)
 
-    stdout, _ = await process.communicate()
-
-    result = stdout.decode().strip()
     return result
 
 async def handle(request, cache):
@@ -40,15 +45,15 @@ async def handle(request, cache):
         ip_to_reach = first_last_ip[1]
         if reg.match(first_ip):
             for i in range(int(start_ip), int(ip_to_reach)+1):
-                result += await scan_nmap(str(base_ip) + '.' + str(i)) + "\n\n"
+                result += await scan_nmap(str(base_ip) + '.' + str(i), cache) + "\n\n"
     else:
-         result += await scan_nmap(to_scan)
+        result += await scan_nmap(to_scan, cache)
 
     # return web.Response(text=str(json.dumps(xmltodict.parse(str(result)))))
     return web.Response(text=str(result))
 
 def main():
-    cache = []
+    cache = {}
 
     loop = asyncio.ProactorEventLoop()
     asyncio.set_event_loop(loop)
